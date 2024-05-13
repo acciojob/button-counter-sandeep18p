ea9e4d8b3d559da38545a139e48d16ecb5bcead0
sandeep18p <sandeep.pansari98@gmail.com>
2024-05-14 01:20:13 +0530

    one

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
+import React, { useState } from 'react';
 
-import React from "react";
-import './../styles/App.css';
+const ButtonCounter = () => {
+  // Initializing counter state using useState hook
+  const [count, setCount] = useState(0);
 
-const App = () => {
+  // Event handler for button click
+  const handleClick = () => {
+    // Incrementing the count by 1
+    setCount(count + 1);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Displaying the count */}
+      <p>Button clicked {count} times.</p>
+      {/* Button with onClick event */}
+      <button onClick={handleClick}>Click Me</button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default ButtonCounter;
